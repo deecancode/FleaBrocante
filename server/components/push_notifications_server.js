cf2cd0f30409a616198baa4dec3b1e281f563d14
nabelou <nabelou@revives-Air.domain>
2017-05-25 22:03:58 -0700

    Add precompiled assets for Heroku

--- server/components/push_notifications_server.js
+++ server/components/push_notifications_server.js
@@ -24,18 +24,18 @@ Meteor.startup(function () {
 Meteor.methods({
   'sendNotification': function(user) {
     check(user, String);
-    
+
     if( !Meteor.settings.fcm.serverKey || !Meteor.settings.public.fcm.senderId ){
 		  if( Meteor.isDevelopment ){
         console.log('Push Notifications are not enabled. Please enter your API keys for Google FCM.')
-      }      
+      }
       return;
     }
 
     Push.send({
-      from: 'TapShop',
-      title: 'TapShop',
-      text: 'You have a new message.',
+      from: 'Fleabrocante',
+      title: 'FleaBrocante',
+      text: 'Vous avez un nouveau message.',
       badge: 1,
       query: {
         userId: user
